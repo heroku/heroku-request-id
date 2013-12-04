71d95c04b4ac2bd78fc1fdfb95deb3dee90b6e32
Mark Fine <mark.fine@gmail.com>
2013-12-04 13:50:05 -0800

    heroku request id plugin.

---
+++ init.rb
@@ -0,0 +1,1 @@
+require File.expand_path("../lib/heroku/request_id", __FILE__)
---
+++ lib/heroku/request_id.rb
@@ -0,0 +1,37 @@
+require "heroku/auth"
+
+Heroku::Command.global_option :request_id, "--request-id", "-z"
+
+module Heroku::RequestId
+  def self.included(base)
+    patch_client(base.client) if base.respond_to? :client
+    patch_api(base.api) if base.respond_to? :api
+  end
+
+  private
+
+  def self.patch_client(client)
+    def client.heroku_headers
+      super.merge(headers)
+    end
+  end
+
+  def self.patch_api(api)
+    api.instance_eval do
+      data = if @connection.respond_to?(:data)
+        @connection.data
+      elsif @connection.respond_to?(:connection)
+        @connection.connection
+      end
+      data[:headers].merge!(headers)
+    end
+  end
+
+  def self.headers
+    headers = {}
+    headers.merge!("Request-Id" => options[:request_id]) if options[:request_id]
+    headers
+  end
+end
+
+Heroku::Auth.send(:include, Heroku::RequestId)
