838e7aa9bd0a44a0a7e6808d8f645d132becfa4a
Dane Harrigan <dane.harrigan@gmail.com>
2013-12-11 17:32:24 -0800

    inject placeholder command for option parsing
    - created a Heroku::RequestId::Placeholder class
    - clone Heroku::Command so that option parsing does mutate the original class
    - trigger prepare_run on the cloned Heroku::Command
    - read from current_options

--- lib/heroku/request_id.rb
+++ lib/heroku/request_id.rb
@@ -1,17 +1,35 @@
 require "heroku/auth"
+require "heroku/command"
 
 Heroku::Command.global_option :request_id, "--request-id REQUESTID", "-z"
 
 module Heroku::RequestId
+  class Placeholder
+    def initialize(*args); end
+    def index; end
+  end
+
   def self.included(base)
     patch_client(base.client) if base.respond_to? :client
     patch_api(base.api) if base.respond_to? :api
+  end
+
+  def self.options
+    cmd = Heroku::Command.clone
+    cmd.commands["request-id"] = {
+      :klass => Heroku::RequestId::Placeholder,
+      :options => [],
+      :method => "index"
+    }
+    cmd.prepare_run("request-id", ARGV)
+    return cmd.current_options
   end
 
   private
 
   def self.patch_client(client)
     def client.heroku_headers
+      options = Heroku::RequestId.options
       headers = {}
       headers.merge!("Request-Id" => options[:request_id]) if options[:request_id]
       super.merge(headers)
@@ -20,6 +38,7 @@ module Heroku::RequestId
 
   def self.patch_api(api)
     api.instance_eval do
+      options = Heroku::RequestId.options
       data = if @connection.respond_to?(:data)
         @connection.data
       elsif @connection.respond_to?(:connection)
